c7c5b91e5ffe536f3959161acab8bc866e366d63
Risyardi Iswanto <elyeel@yahoo.com>
2021-02-22 22:51:55 -0800

    set graphiql to work with mongodb server at mongo atlas

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 4000;
+
+mongoose.connect(
+	'mongodb+srv://risyardi:test@cluster0.rw7we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
+mongoose.connection.once('open', () => {
+	console.log('connected to database');
+});
 
 app.use(
 	'/graphql',
