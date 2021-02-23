ec799b03dc9e9bcbce1bbcaa1f92e7bea351931a
Risyardi Iswanto <elyeel@yahoo.com>
2021-02-22 18:18:33 -0800

    server setup with dummy data and graphql server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+const port = 4000;
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
+
+app.listen(port, () => {
+	console.log(`now listening for request on port ${port}`);
+});
